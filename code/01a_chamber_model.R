f51c91763dfe4908ee5a919c110a59ada3a62d44
Sz-Tim <tim.szewczyk@gmail.com>
2022-07-20 09:27:45 +0100

    prior adjustment

--- code/01a_chamber_model.R
+++ code/01a_chamber_model.R
@@ -57,20 +57,21 @@ priors <- c(prior(normal(0, 2), class="b"),
             prior(normal(0, 2), class="Intercept"),
             prior(cauchy(0, 2), class="sd"))
 prior.nl <- c(prior(normal(0, 1), class="b", nlpar="A", lb=0),
-              prior(normal(3, 2), class="b", nlpar="M", lb=0),
+              prior(normal(1, 1), class="b", nlpar="M", lb=0),
               prior(uniform(0, 24), class="b", nlpar="phi", lb=0, ub=24),
               prior(normal(0, 0.1), class="sd", nlpar="A", lb=0),
               prior(normal(0, 0.1), class="sd", nlpar="M", lb=0),
               prior(normal(0, 0.1), class="sd", nlpar="phi", lb=0),
-              prior(normal(0, 0.1), "sigma", lb=0))
+              prior(normal(0, 0.1), dpar="sigma", lb=0))
 
 
 # fit model ---------------------------------------------------------------
 
 out.nl <- brm(bf(ln_FishCount ~ M + A * cos(3.141593*(ZT + phi)/12),
-                 M ~ 1 + Group*Chamber + (1+Group*Chamber|Tank), 
+                 M ~ 0 + Group*Chamber + (0+Group*Chamber|Tank), 
                  A ~ 0 + Group*Chamber + (0+Group*Chamber|Tank), 
                  phi ~ 0 + Group*Chamber + (0+Group*Chamber|Tank),
+                 sigma ~ Group,
                  nl=TRUE),
               prior=prior.nl, 
               control=stan_args,
